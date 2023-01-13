7285768eb2a0428670d3e5305140d711bab2a2c7
Raef Coles <raef.coles@arm.com>
2023-01-13 09:26:47 +0000

    rss: Enable RSS images in FIPs
    Remove RSS flash creation and instead insert the images into FIPs. Move
    GPT creation into build-rss.sh so it can be run after the RSS images are
    inserted. Change the alignment of the GPT partitions to 8KiB, to match
    the required alignment for the RSS ATU/SIC, and patch how the GPT image
    is built to allows for partitions that are aligned to 8KiB boundaries.
    
    Signed-off-by: Raef Coles <raef.coles@arm.com>
    Change-Id: Ic421ad61ad0639e8832f233df46627403503f2fb

--- build-flash-image.sh
+++ build-flash-image.sh
@@ -28,11 +28,109 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-do_build() {
-    true
+do_generate_gpt() {
+# $1 ... Input FIP file
+# $2 ... Output GPT image file
+
+    OUTDIR=$OUTPUT_DIR/tmp_build/flash-image
+
+    BUILD_TYPE=release
+    if [[ $TFA_DEBUG -eq 1 ]]; then
+        BUILD_TYPE=debug
+    fi
+
+    fip_bin="$1"
+    gpt_image="$2"
+    # the FIP partition type is not standardized, so generate one
+    fip_type_uuid=`uuidgen --sha1 --namespace @dns --name "fip_type_uuid"`
+    # metadata partition type UUID, specified by the document:
+    # Platform Security Firmware Update for the A-profile Arm Architecture
+    # version: 1.0BET0
+    metadata_type_uuid="8a7a84a0-8387-40f6-ab41-a8b9a5a60d23"
+    location_uuid=`uuidgen`
+    FIP_A_uuid=`uuidgen`
+    FIP_B_uuid=`uuidgen`
+
+    # maximum FIP size 12MiB. This is the current size of the FIP rounded up to an integer number of MiB.
+    fip_max_size=12582912
+    fip_bin_size=$(stat -c %s $fip_bin)
+    if [ $fip_max_size -lt $fip_bin_size ]; then
+        bberror "fip.bin ($fip_bin_size bytes) is larger than the GPT partition ($fip_max_size bytes)"
+    fi
+
+    # maximum metadata size 512B. This is the current size of the metadata rounded up to an integer number of sectors.
+    metadata_max_size=512
+    metadata_file="${OUTDIR}/metadata.bin"
+    python3 $TFA_FILES/generate_metadata.py --metadata_file $metadata_file \
+                                            --img_type_uuids $fip_type_uuid \
+                                            --location_uuids $location_uuid \
+                                            --img_uuids $FIP_A_uuid $FIP_B_uuid
+
+    # create GPT image. The GPT contains 2 FIP partitions: FIP_A and FIP_B, and 2 metadata partitions: FWU-Metadata and Bkup-FWU-Metadata.
+    # the GPT layout is the following:
+    # -----------------------
+    # Protective MBR
+    # -----------------------
+    # Primary GPT Header
+    # -----------------------
+    # FIP_A
+    # -----------------------
+    # FIP_B
+    # -----------------------
+    # FWU-Metadata
+    # -----------------------
+    # Bkup-FWU-Metadata
+    # -----------------------
+    # Secondary GPT Header
+    # -----------------------
+
+    sector_size=512
+    gpt_header_size=33 # valid only for 512-byte sectors
+    num_sectors_fip=`expr $fip_max_size / $sector_size`
+    num_sectors_metadata=`expr $metadata_max_size / $sector_size`
+    start_sector_1=`expr 1 + $gpt_header_size` # size of MBR is 1 sector
+    start_sector_2=`expr $start_sector_1 + $num_sectors_fip`
+    start_sector_3=`expr $start_sector_2 + $num_sectors_fip`
+    start_sector_4=`expr $start_sector_3 + $num_sectors_metadata`
+    num_sectors_gpt=`expr $start_sector_4 + $num_sectors_metadata + $gpt_header_size`
+    gpt_size=`expr $num_sectors_gpt \* $sector_size`
+
+    # create raw image
+    dd if=/dev/zero of=$gpt_image bs=$gpt_size count=1
+
+    # create the GPT layout
+    sgdisk $gpt_image \
+           --set-alignment 16 \
+           --disk-guid $location_uuid \
+           \
+           --new 1:$start_sector_1:+$num_sectors_fip \
+           --change-name 1:FIP_A \
+           --typecode 1:$fip_type_uuid \
+           --partition-guid 1:$FIP_A_uuid \
+           \
+           --new 2:$start_sector_2:+$num_sectors_fip \
+           --change-name 2:FIP_B \
+           --typecode 2:$fip_type_uuid \
+           --partition-guid 2:$FIP_B_uuid \
+           \
+           --new 3:$start_sector_3:+$num_sectors_metadata \
+           --change-name 3:FWU-Metadata \
+           --typecode 3:$metadata_type_uuid \
+           \
+           --new 4:$start_sector_4:+$num_sectors_metadata \
+           --change-name 4:Bkup-FWU-Metadata \
+           --typecode 4:$metadata_type_uuid
+
+    # populate the GPT partitions
+    dd if=$fip_bin of=$gpt_image bs=$sector_size seek=$(gdisk -l $gpt_image | grep " FIP_A$" | awk '{print $2}') count=$num_sectors_fip conv=notrunc
+    dd if=$fip_bin of=$gpt_image bs=$sector_size seek=$(gdisk -l $gpt_image | grep " FIP_B$" | awk '{print $2}') count=$num_sectors_fip conv=notrunc
+    dd if=$metadata_file of=$gpt_image bs=$sector_size seek=$(gdisk -l $gpt_image | grep " FWU-Metadata$" | awk '{print $2}') count=$num_sectors_metadata conv=notrunc
+    dd if=$metadata_file of=$gpt_image bs=$sector_size seek=$(gdisk -l $gpt_image | grep " Bkup-FWU-Metadata$" | awk '{print $2}') count=$num_sectors_metadata conv=notrunc
+
+    info_echo "Built $gpt_image"
 }
 
-do_patch() {
+do_build() {
     true
 }
 
@@ -43,25 +141,33 @@ do_clean() {
     rm -rf $OUTDIR
 }
 
-assemble_combined_flash() {
-# $1 ... FIP input file
-# $2 ... RSS flash input file
-# $3 ... combined flash output file
+do_patch() {
+    true
+}
 
-    FIP_MAX_SIZE=35651584 #34 MiB
-    FIP_SIZE=$(wc -c $1 | awk '{print $1}')
+assemble_fip() {
+# $1 ... Input FIP file
+# $2 ... Output FIP file
 
-    if [ $FIP_SIZE -gt $FIP_MAX_SIZE ]
-    then
-        die "Error: $1 size ($FIP_SIZE) is more than supported($FIP_MAX_SIZE)"
+    cp $1 $2
+
+    FIPTOOL=$TFA_SRC/tools/fiptool/fiptool
+
+    $FIPTOOL update --align 8192 --rss-bl2 $RSS_BINDIR/bl2_signed.bin $2
+    $FIPTOOL update --align 8192 --rss-scp-bl1 $RSS_BINDIR/signed_$RSS_SIGN_SCP_BL1_NAME $2
+    $FIPTOOL update --align 8192 --rss-ap-bl1 $RSS_BINDIR/signed_$RSS_SIGN_AP_BL1_NAME $2
+
+    if [[ -e ${RSS_BINDIR}/tfm_s_sic_tables_signed.bin ]]; then
+        $FIPTOOL update --align 8192 --rss-ns $RSS_BINDIR/tfm_ns.bin $2
+        $FIPTOOL update --align 8192 --rss-s $RSS_BINDIR/tfm_s.bin $2
+        $FIPTOOL update --align 8192 --rss-sic-tables-ns $RSS_BINDIR/tfm_ns_sic_tables_signed.bin $2
+        $FIPTOOL update --align 8192 --rss-sic-tables-s $RSS_BINDIR/tfm_s_sic_tables_signed.bin $2
+    else
+        $FIPTOOL update --align 8192 --rss-ns $RSS_BINDIR/tfm_ns_signed.bin $2
+        $FIPTOOL update --align 8192 --rss-s $RSS_BINDIR/tfm_s_signed.bin $2
     fi
 
-    srec_cat \
-        $1 -Binary -offset 0x0 \
-        $2 -Binary -offset 0x2200000 \
-        -o $3 -Binary
-
-    info_echo "Created $3"
+    info_echo "Built $2"
 }
 
 do_deploy() {
@@ -73,37 +179,34 @@ do_deploy() {
     OUTDIR=$OUTPUT_DIR/tmp_build/flash-image
     mkdir -p $OUTDIR
 
+    BUILD_TYPE=release
+    if [[ $TFA_DEBUG -eq 1 ]]; then
+        BUILD_TYPE=debug
+    fi
+
     RSS_SIGN_AP_BL1_NAME=$RSS_SIGN_AP_BL1_NAME_BUILDROOT
     if [[ $FILESYSTEM == "android-swr" ]]; then
         RSS_SIGN_AP_BL1_NAME=$RSS_SIGN_AP_BL1_NAME_ANDROID
     fi
 
-    srec_cat \
-        $RSS_BINDIR/bl2_signed.bin -Binary -offset 0x0 \
-        $RSS_BINDIR/bl2_signed.bin -Binary -offset 0x10000 \
-        $RSS_BINDIR/tfm_s_ns_signed.bin -Binary -offset 0x20000 \
-        $RSS_BINDIR/tfm_s_ns_signed.bin -Binary -offset 0xE0000 \
-        $RSS_BINDIR/signed_${RSS_SIGN_AP_BL1_NAME} -Binary -offset 0x1A0000 \
-        $RSS_BINDIR/signed_${RSS_SIGN_SCP_BL1_NAME} -Binary -offset 0x220000 \
-        $RSS_BINDIR/signed_${RSS_SIGN_AP_BL1_NAME} -Binary -offset 0x2A0000 \
-        $RSS_BINDIR/signed_${RSS_SIGN_SCP_BL1_NAME} -Binary -offset 0x320000 \
-        -o $OUTDIR/rss_flash.bin -Binary
+    assemble_fip $TFA_OUTDIR/build/$TFA_PLATFORM/$BUILD_TYPE/fip.bin \
+                 ${OUTDIR}/fip_combined.bin
 
-    assemble_combined_flash \
-        $TFA_OUTDIR/build/$TFA_PLATFORM/$BUILD_TYPE/fip.bin \
-        $OUTDIR/rss_flash.bin \
-        $OUTDIR/combined_flash.bin
-
-    ln -sf $OUTDIR/combined_flash.bin $DEPLOY_DIR/$PLATFORM/combined_flash.bin
+    ln -sf $OUTDIR/fip_combined.bin $DEPLOY_DIR/$PLATFORM/fip-tc.bin 2>/dev/null || :
+    if [[ $TC_FWU_SUPPORT -eq 1 ]]; then
+        do_generate_gpt $OUTDIR/fip_combined.bin $OUTDIR/fip_gpt.bin
+        ln -sf $OUTDIR/fip_gpt.bin $DEPLOY_DIR/$PLATFORM/fip_gpt-tc.bin 2>/dev/null || :
+    fi
 
     if [[ -e $TRUSTY_OUTDIR/build/tc/debug/fip.bin ]]; then
-        assemble_combined_flash \
-            $TRUSTY_OUTDIR/build/tc/debug/fip.bin \
-            $OUTDIR/rss_flash.bin \
-            $OUTDIR/combined_flash_trusty.bin
+        assemble_fip $TRUSTY_OUTDIR/build/tc/debug/fip.bin \
+                     ${OUTDIR}/fip_combined_trusty.bin
 
-        ln -sf $OUTDIR/combined_flash_trusty.bin \
-               $DEPLOY_DIR/$PLATFORM/combined_flash_trusty.bin
+        ln -sf $OUTDIR/fip_combined_trusty.bin $DEPLOY_DIR/$PLATFORM/fip-trusty-tc.bin 2>/dev/null || :
+        if [[ $TC_FWU_SUPPORT -eq 1 ]]; then
+            do_generate_gpt $OUTDIR/fip_combined.bin $OUTDIR/fip_gpt_trusty.bin
+            ln -sf $OUTDIR/fip_gpt_trusty.bin $DEPLOY_DIR/$PLATFORM/fip_gpt-trusty-tc.bin 2>/dev/null || :
+        fi
     fi
 }
 
--- build-tfa.sh
+++ build-tfa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2022, Arm Limited. All rights reserved.
+# Copyright (c) 2022-2023, Arm Limited. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -39,106 +39,6 @@ do_build() {
     make "${make_opts_tfa[@]}" "${make_opts_tfa_optee[@]}" all fip
     popd
 
-    if [[ $TC_FWU_SUPPORT -eq 1 ]]; then
-        do_generate_gpt
-    fi
-
-}
-
-do_generate_gpt() {
-    BUILD_TYPE=release
-    if [[ $TFA_DEBUG -eq 1 ]]; then
-        BUILD_TYPE=debug
-    fi
-
-    OUTDIR=$TFA_OUTDIR/build/tc/$BUILD_TYPE
-    gpt_image="${OUTDIR}/fip_gpt.bin"
-    fip_bin="${OUTDIR}/fip.bin"
-    # the FIP partition type is not standardized, so generate one
-    fip_type_uuid=`uuidgen --sha1 --namespace @dns --name "fip_type_uuid"`
-    # metadata partition type UUID, specified by the document:
-    # Platform Security Firmware Update for the A-profile Arm Architecture
-    # version: 1.0BET0
-    metadata_type_uuid="8a7a84a0-8387-40f6-ab41-a8b9a5a60d23"
-    location_uuid=`uuidgen`
-    FIP_A_uuid=`uuidgen`
-    FIP_B_uuid=`uuidgen`
-
-    # maximum FIP size 4MB. This is the current size of the FIP rounded up to an integer number of MB.
-    fip_max_size=4194304
-    fip_bin_size=$(stat -c %s $fip_bin)
-    if [ $fip_max_size -lt $fip_bin_size ]; then
-        bberror "FIP binary ($fip_bin_size bytes) is larger than the GPT partition ($fip_max_size bytes)"
-    fi
-
-    # maximum metadata size 512B. This is the current size of the metadata rounded up to an integer number of sectors.
-    metadata_max_size=512
-    metadata_file="${OUTDIR}/metadata.bin"
-    python3 $TFA_FILES/generate_metadata.py --metadata_file $metadata_file \
-                                            --img_type_uuids $fip_type_uuid \
-                                            --location_uuids $location_uuid \
-                                            --img_uuids $FIP_A_uuid $FIP_B_uuid
-
-    # create GPT image. The GPT contains 2 FIP partitions: FIP_A and FIP_B, and 2 metadata partitions: FWU-Metadata and Bkup-FWU-Metadata.
-    # the GPT layout is the following:
-    # -----------------------
-    # Protective MBR
-    # -----------------------
-    # Primary GPT Header
-    # -----------------------
-    # FIP_A
-    # -----------------------
-    # FIP_B
-    # -----------------------
-    # FWU-Metadata
-    # -----------------------
-    # Bkup-FWU-Metadata
-    # -----------------------
-    # Secondary GPT Header
-    # -----------------------
-
-    sector_size=512
-    gpt_header_size=33 # valid only for 512-byte sectors
-    num_sectors_fip=`expr $fip_max_size / $sector_size`
-    num_sectors_metadata=`expr $metadata_max_size / $sector_size`
-    start_sector_1=`expr 1 + $gpt_header_size` # size of MBR is 1 sector
-    start_sector_2=`expr $start_sector_1 + $num_sectors_fip`
-    start_sector_3=`expr $start_sector_2 + $num_sectors_fip`
-    start_sector_4=`expr $start_sector_3 + $num_sectors_metadata`
-    num_sectors_gpt=`expr $start_sector_4 + $num_sectors_metadata + $gpt_header_size`
-    gpt_size=`expr $num_sectors_gpt \* $sector_size`
-
-    # create raw image
-    dd if=/dev/zero of=$gpt_image bs=$gpt_size count=1
-
-    # create the GPT layout
-    sgdisk $gpt_image \
-           --set-alignment 1 \
-           --disk-guid $location_uuid \
-           \
-           --new 1:$start_sector_1:+$num_sectors_fip \
-           --change-name 1:FIP_A \
-           --typecode 1:$fip_type_uuid \
-           --partition-guid 1:$FIP_A_uuid \
-           \
-           --new 2:$start_sector_2:+$num_sectors_fip \
-           --change-name 2:FIP_B \
-           --typecode 2:$fip_type_uuid \
-           --partition-guid 2:$FIP_B_uuid \
-           \
-           --new 3:$start_sector_3:+$num_sectors_metadata \
-           --change-name 3:FWU-Metadata \
-           --typecode 3:$metadata_type_uuid \
-           \
-           --new 4:$start_sector_4:+$num_sectors_metadata \
-           --change-name 4:Bkup-FWU-Metadata \
-           --typecode 4:$metadata_type_uuid
-
-    # populate the GPT partitions
-    dd if=$fip_bin of=$gpt_image bs=$sector_size seek=$start_sector_1 count=$num_sectors_fip conv=notrunc
-    dd if=$fip_bin of=$gpt_image bs=$sector_size seek=$start_sector_2 count=$num_sectors_fip conv=notrunc
-    dd if=$metadata_file of=$gpt_image bs=$sector_size seek=$start_sector_3 count=$num_sectors_metadata conv=notrunc
-    dd if=$metadata_file of=$gpt_image bs=$sector_size seek=$start_sector_4 count=$num_sectors_metadata conv=notrunc
 }
 
 
@@ -159,10 +59,6 @@ do_deploy() {
 
     ln -s $TFA_OUTDIR/build/$TFA_PLATFORM/$BUILD_TYPE/bl1.bin $DEPLOY_DIR/$PLATFORM/bl1-tc.bin 2>/dev/null || :
     ln -s $TFA_OUTDIR/build/$TFA_PLATFORM/$BUILD_TYPE/bl1/bl1.elf $DEPLOY_DIR/$PLATFORM/bl1-tc.elf 2>/dev/null || :
-    ln -s $TFA_OUTDIR/build/$TFA_PLATFORM/$BUILD_TYPE/fip.bin $DEPLOY_DIR/$PLATFORM/fip-tc.bin 2>/dev/null || :
-    if [[ $TC_FWU_SUPPORT -eq 1 ]]; then
-        ln -s $TFA_OUTDIR/build/$TFA_PLATFORM/$BUILD_TYPE/fip_gpt.bin $DEPLOY_DIR/$PLATFORM/fip_gpt-tc.bin 2>/dev/null || :
-    fi
 }
 
 do_patch() {
