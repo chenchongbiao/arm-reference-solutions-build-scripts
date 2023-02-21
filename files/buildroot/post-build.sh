f9fe4f10bf9784964c9f427ca47505de1acfd8c0
Davidson K <davidson.kumaresan@arm.com>
2023-02-21 17:03:16 +0530

    buildroot: mount mpamfs automatically during the boot
    Change-Id: I674e074a2e737d3c6ca188bf46fac432b2b0e03e
    Signed-off-by: Davidson K <davidson.kumaresan@arm.com>

--- files/buildroot/post-build.sh
+++ files/buildroot/post-build.sh
@@ -1,1 +1,2 @@
 echo "debugfs    /sys/kernel/debug      debugfs  defaults  0 0" >> ${TARGET_DIR}/etc/fstab
+echo "mpamfs     /sys/fs/mpam           mpam     defaults  0 0" >> ${TARGET_DIR}/etc/fstab
